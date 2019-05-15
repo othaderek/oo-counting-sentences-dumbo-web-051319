335ba9d02e4c550831004c325def4df80cddaa7a
Otha D Hernandez <otha.hernandez@gmail.com>
2019-05-15 18:38:40 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,12 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     # We run split method on self, regexp,delete
     self.split(/\.|\?|\!/).delete_if {|i| i.length < 2}.length
 
+=======
+    self.split(/\.|\?|\!/).delete_if {|w| w.length < 2}.length
+>>>>>>> 9f5988d97be5163861badf2e845b936e98db0939
   end
 end
